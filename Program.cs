319699011518105570b23d0c8a574a9c3d081739
RKDeriya <141213684+RKDeriya@users.noreply.github.com>
2023-08-21 18:47:16 +0530

    PalindromOrNot

--- Program.cs
+++ Program.cs
@@ -1,99 +1,53 @@
-using JioMartGeneric;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JioMartGeneric
+namespace Palindrom
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Base<DateTime,DateTime> objbase = new Base<DateTime,DateTime>();
-            objbase.CreatedOn = DateTime.Now;
-            objbase.UpdatedOn = DateTime.Now;
+            /*
+            Console.Write("Enter the string for create Palindrome:");
+            string check = Console.ReadLine();
+            int i = check.Length;
+            while(i> 0)
+            {
+                Console.Write(check.ElementAt(i-1));
+                i--;
+            }
+            Console.ReadKey();
+            */
 
-            UserDetails objUserDetails = new UserDetails();
-            objUserDetails.UserDetailsId = 1;
-            objUserDetails.UserFirstName = "Aman";
-            objUserDetails.UserLastName = "Gupta";
-            objUserDetails.UserAddress = "Kalol,Gandhinagar";
-            objUserDetails.UserMobileNo = "123456789";
-            objUserDetails.UserWalletBalance = 0;
-            objbase.CreatedOn = DateTime.Now;
-            objbase.UpdatedOn = DateTime.Now;
-
-
-            Console.WriteLine($"UserDetailsId:{objUserDetails.UserDetailsId}");
-            Console.WriteLine($"UserFirstname:{objUserDetails.UserFirstName}");
-            Console.WriteLine($"Userlastname:{objUserDetails.UserLastName}");
-            Console.WriteLine($"UserAdress:{objUserDetails.UserAddress}");
-            Console.WriteLine($"UserMobileNo:{objUserDetails.UserMobileNo}");
-            Console.WriteLine($"UserMobileNo:{objUserDetails.UserMobileNo}");
-            Console.WriteLine($"UserWalletBalance:{objUserDetails.UserWalletBalance}");
-            Console.WriteLine($"CreatedOn:{objUserDetails.CreatedOn}");
-            Console.WriteLine($"UpdatedOn:{objUserDetails.UpdatedOn}");
+            Console.Write("Enter the string for identifying for the Palindrome or not:");
+            string check = Console.ReadLine();
+            string reverse = string.Empty;
+            if (check.Length >0)
+            {
+                for(int i = check.Length -1; i>=0; i--)
+                {
+                    reverse += check[i].ToString();
+                }
+                if (reverse == check)
+                {
+                    Console.WriteLine("Entered string is palindrom");
+                }
+                else
+                {
+                    Console.WriteLine("Entered string is not palindrom");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter the string");
+            }
+           
             Console.ReadKey();
         }
-    }
 
-    public class Base<T,I>
-    {
-        public T CreatedOn { get; set; }
-        public I UpdatedOn { get; set; }
-    }
-    public class UserDetails : Base<DateTime,DateTime>
-    {
-
-        public int UserDetailsId { get; set; }
-        public string UserFirstName { get; set; }
-        public string UserLastName { get; set; }
-        public string UserAddress { get; set; }
-        public string UserMobileNo { get; set; }
-        public float UserWalletBalance { get; set; }
-    }
-
-    public class Category : Base<DateTime, DateTime>
-    {
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-    }
-
-    public class ProductDetails : Base<DateTime, DateTime>
-    {
-        public int ProductDetailsId { get; set; }
-        public string ProductName { get; set; }
-        public float ProductPrice { get; set; }
-        public int ProductStockQuantity { get; set; }
-        public int CategoryId { get; set; }
 
     }
-
-    public class CartDetails : Base<DateTime, DateTime>
-    {
-        public int CartDetailsId { get; set; }
-        public int ProductDetailsId { get; set; }
-        public int ProductQuantity
-        {
-            get; set;
-        }
-        public float CartTotalPrice { get; set; }
-        public string CartOrderStatus { get; set; } = "Not Complete";
-        public int UserDetailsId { get; set; }
-
-    }
-
-    public class OrderDetails : Base<DateTime, DateTime>
-    {
-        public int OrderDetailsId { get; set; }
-        public int CartDetailsId { get; set; }
-        public string OrderPaymentMode { get; set; }
-        public int OrderTotalPrice { get; set; }
-        public string OrderDeliveryStatus { get; set; } = "InProgress";
-        public DateTime OrderDate { get; set; }
-
-    }
-
 }
